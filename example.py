490b25bcfcab1f8a20b8a884f9097ea6839ace55
Medlion <bvwyngaard@gmail.com>
2024-09-20 20:12:00 +0200

    Slow down servo

--- example.py
+++ example.py
@@ -88,12 +88,12 @@ if __name__=='__main__':
   while True:
    # setServoPulse(2,2500)
     #for i in range(500,2500,10):
-    for i in range(0, 2048, 10):
+    for i in range(0, 2048, 5):
       pwm.setServoPulse(0,i)
       time.sleep(0.02)
 
     #for i in range(2500,500,-10):
-    for i in range(2048,0,-10):
+    for i in range(2048,0,-5):
       pwm.setServoPulse(0,i)
       time.sleep(0.02)
 
