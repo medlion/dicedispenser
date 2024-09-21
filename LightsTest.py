6008542457f5792d5e1786831473b17f17fadd9c
Medlion <bvwyngaard@gmail.com>
2024-09-21 20:19:56 +0200

    Update test for 40 LEDs

--- LightsTest.py
+++ LightsTest.py
@@ -4,10 +4,11 @@ import time
 if __name__ == "__main__":
 
     # Initialize the WS2812 strip with 100 leds and SPI channel 0, CE0
-    strip = WS2812SpiDriver(spi_bus=0, spi_device=0, led_count=8).get_strip()
+    strip = WS2812SpiDriver(spi_bus=0, spi_device=0, led_count=40).get_strip()
     while True:
         strip.set_all_pixels(Color(255, 0, 0))
         strip.show()
+        strip
         time.sleep(2)
         strip.set_all_pixels(Color(0, 255, 0))
         strip.show()
