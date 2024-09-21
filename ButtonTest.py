cbb41dad7297a44de2bf24be135da43577ab4d0b
Medlion <bvwyngaard@gmail.com>
2024-09-21 09:07:53 +0200

    Other button press

--- ButtonTest.py
+++ ButtonTest.py
@@ -1,8 +1,5 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+from gpiozero import Button
+button = Button(2)
 
-while True: # Run forever
-    if GPIO.input(8) == GPIO.HIGH:
-        print("Button was pushed!")
+button.wait_for_press()
+print('You pushed me')
