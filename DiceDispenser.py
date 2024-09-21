f7554a4c1e79df4ee7f486b3377dbd5cf6764810
Medlion <bvwyngaard@gmail.com>
2024-09-21 15:10:26 +0200

    Startup

--- DiceDispenser.py
+++ DiceDispenser.py
@@ -5,17 +5,24 @@ from gpiozero import Button
 NUMBER_OF_SERVOS = 5
 BUTTON_GPIO = 14
 
+PCA = PCA9685.PCA9685() # Control for the servo motors
+
 def getRandomNumber(max): # generate a random number between 0 and max-1
     return random.randint(0, max-1)
 
+def goServoGo(channel): # Putting this in it's own function to standardise LED lighting and such
+    PCA.spinChannel(channel=channel)
+
 def onButtonPress():
     chosenServo = getRandomNumber(NUMBER_OF_SERVOS)
-    print(chosenServo)
-    PCA = PCA9685.PCA9685() # Control for the servo motors
-    PCA.spinChannel(channel=chosenServo)
-
+    goServoGo(chosenServo)
+    
+def startup():
+    for i in range(NUMBER_OF_SERVOS):
+        goServoGo(i)
 
 if __name__=='__main__':
+    startup()
     
     button = Button(BUTTON_GPIO)
 
