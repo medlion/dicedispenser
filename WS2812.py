dc60a5a46a5e3cae1b803526e64901e73e2c410b
Bernard van Wyngaard <bernard.wyngaard@afrihost.com>
2025-08-18 19:45:02 +0200

    Set brightness

--- WS2812.py
+++ WS2812.py
@@ -5,13 +5,14 @@ class WS2812:
     driver = 0
     ledsPerUnit = 0
     units = 0
+    BRIGHTNESS = 1 # Value between 0 and 1
 
     def __init__(self, ledsPerUnit, units) -> None:
         self.ledsPerUnit = ledsPerUnit
         self.units = units
         ledCount = ledsPerUnit*units
         self.driver = WS2812SpiDriver(spi_bus=0, spi_device=0, led_count=ledCount).get_strip()
-        self.driver.set_brightness(0.25)
+        self.driver.set_brightness(self.BRIGHTNESS)
 
     def clear(self) -> None:
         self.driver.clear()
